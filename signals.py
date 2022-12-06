8096474621a165032e4f4375121040210c3f97cb
Cooper <scoop.jackson1234@gmail.com>
2022-12-06 13:57:44 -0600

    AoC Day 6

---
+++ signals.py
@@ -0,0 +1,35 @@
+
+#the start of a packet is indicated by a sequence of four characters that are all different.
+
+
+#bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+#nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+#nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+#zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
+
+#part 1 and 2
+        
+def unique_elements(num, line):
+    buffer = [''] * num
+    differ = False
+    for i, c in enumerate(line):
+        buffer.pop(0)
+        buffer.append(c)
+        for z in buffer:
+            if buffer.count(z) > 1 :
+                differ = False
+                break
+            if buffer[0] != '':
+                differ = True
+        
+        if differ:
+            print(i + 1)
+            break
+with open('packet_stream.txt') as file:
+    line = file.readline()
+    unique_elements(4,line)
+    unique_elements(14,line)
+
+
+
+
